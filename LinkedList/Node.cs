89aa757583beba83024012f2c09cf83101c1aa5a
Anton Churkin <afrg@yandex.ru>
2020-09-26 15:55:08 +0500

    init

---
+++ LinkedList/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Node<T>
+    {
+        public Node(T data, Node<T> next)
+        {
+            Value = data;
+            Next = next;
+        }
+
+        public Node<T> Next
+        {
+            get;
+            private set;
+        }
+
+        public T Value
+        {
+            get;
+            private set;
+        }
+    }
+}
